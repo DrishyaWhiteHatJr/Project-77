72b4817c6a2606a8b96f21eb9a8920e8dcb86b1c
DrishyaWhiteHatJr <77913088+DrishyaWhiteHatJr@users.noreply.github.com>
2021-04-24 16:45:58 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_Name = "Drishya"
+var last_Name = "Sharma"
+
+var full_name= first_Name.concat(last_Name);
+console.log(full_name);
+
+var number_1 = 10
+var number_2 = 5
+console.log( number_1 + number_2);
+console.log(number_1 - number_2);
+console.log(number_1 * number_2);
+console.log(number_1 / number_2);
